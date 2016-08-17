bc99c3f8dffb48080f5431d8293f14f3d24bc116
zhufengnodejs <zhufengnodejs@126.com>
2016-08-17 16:33:20 +0800

    module

--- proxy/client.js
+++ proxy/client.js
@@ -1,23 +1,26 @@
 var http = require('http');
+http.createServer(function(req,res){
 //当调用request的时候请求并不会发出
-var req = http.request({
-  host:'localhost',//主机名
-  method:'GET',//请求的方法
-  port:9090,//端口号
-  path:'/',//路径
-  headers:{}//请求头
-},function(response){
-    //response是一个可读可写流
-    var res  = '';
-    response.setEncoding('utf8');
-    response.on('data',function(data){
-        res+=data;
-    });
-    response.on('data',function(data){
-        console.log(res);
-    });
-})
+    var request = http.request({
+        host:'localhost',//主机名
+        method:'GET',//请求的方法
+        port:9090,//端口号
+        path:'/',//路径
+        headers:{}//请求头
+    },function(response){
+        //response是一个可读可写流
+        var result  = '';
+        response.setEncoding('utf8');
+        response.on('data',function(data){
+            result+=data;
+        });
+        response.on('end',function(data){
+           // console.log(result);
+            res.end(result);
+        });
+    })
 //表示写入请求体
-req.write('xxx');//可写流
+    //request.write('xxx');//可写流
 //结束请求 这时才真正向服务器发送了请求
-req.end();
+    request.end();
+}).listen(8088);
