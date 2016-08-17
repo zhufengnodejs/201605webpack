1971ff1f7b718b61236f233980cc74d4a5eecccf
zhufengnodejs <zhufengnodejs@126.com>
2016-08-17 16:43:24 +0800

    module

--- proxy/client.js
+++ proxy/client.js
@@ -1,26 +1,37 @@
 var http = require('http');
 http.createServer(function(req,res){
+    var urlObj = require('url').parse(req.url);
+    var pathname = urlObj.pathname;
+    if(pathname=='/api/books'){
 //当调用request的时候请求并不会发出
-    var request = http.request({
-        host:'localhost',//主机名
-        method:'GET',//请求的方法
-        port:9090,//端口号
-        path:'/',//路径
-        headers:{}//请求头
-    },function(response){
-        //response是一个可读可写流
-        var result  = '';
-        response.setEncoding('utf8');
-        response.on('data',function(data){
-            result+=data;
-        });
-        response.on('end',function(data){
-           // console.log(result);
-            res.end(result);
-        });
-    })
+        var request = http.request({
+            host:'localhost',//主机名
+            port:9090,//端口号
+            method:'GET',//请求的方法
+            path:rewrite(pathname),//路径
+            headers:{}//请求头
+        },function(response){
+            //response是一个可读可写流
+            var result  = '';
+            response.setEncoding('utf8');
+            response.on('data',function(data){
+                result+=data;
+            });
+            response.on('end',function(data){
+                // console.log(result);
+                res.end(result);
+            });
+        })
 //表示写入请求体
-    //request.write('xxx');//可写流
+        //request.write('xxx');//可写流
 //结束请求 这时才真正向服务器发送了请求
-    request.end();
+        request.end();
+    }else {
+        res.end('404');
+    }
+
 }).listen(8088);
+
+function rewrite(pathname){
+
+}
--- proxy/server.js
+++ proxy/server.js
@@ -1,4 +1,11 @@
 var http = require('http');
 http.createServer(function(req,res){
-    res.end('9090');
+    var urlObj = require('url').parse(req.url);
+    var pathname = urlObj.pathname;
+    if(pathname=='/books.json'){
+        res.end(JSON.stringify([{id:1,name:'node.js'}]));
+    }else{
+        res.end('404');
+    }
+
 }).listen(9090);
