fcaa92b5058b71ff3b26f23f0a19131563666114
zhufengnodejs <zhufengnodejs@126.com>
2016-08-31 15:28:44 +0800

    提取公共代码

---
+++ react/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+//把这个H1标签插入到APP的DIV内部
+ReactDOM.render(
+    <h1>珠峰培训</h1>,
+    document.querySelector('#app')
+);
---
+++ webpack.config.react.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
+module.exports = {
+    //指定打包的入口文件是react/js
+    entry:path.resolve('./react/index.js'),
+    //输出设置
+    output:{
+        //输出的目录
+        path:'./build',
+        //文件名
+        filename:'bundle.js'
+    },
+    //配置模块
+    module:{
+        //设置加载器是一个数组 s
+        loaders:[
+            {
+                //匹配 js 和jsx后缀
+                test:/\.jsx?$/,
+                //babel是一个通用编译器，默认什么都不做
+                loader:'babel'
+            }
+        ]
+    },
+    plugins:[
+        //用来在build目录下生成html文件
+        new HtmlWebpackPlugin({
+            template:'./react/index.html'
+        }),
+        //当打包成功之后自动打开浏览器显示url地址
+        new OpenBrowserWebpackPlugin({
+            url:'http://localhost:8080'
+        })
+    ]
+
+}
