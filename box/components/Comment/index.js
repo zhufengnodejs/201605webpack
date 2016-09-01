380350dc605a76d614fbea21f04ab5ef25132dc0
zhufengnodejs <zhufengnodejs@126.com>
2016-09-01 15:43:08 +0800

    实现时间提示

--- box/components/Comment/index.js
+++ box/components/Comment/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+require('moment/locale/zh-cn.js');
+var moment = require('moment');
+//moment().fromNow()
 export default class Comment extends React.Component {
     remove() {
         this.props.removeComment(this.props.id);
@@ -7,7 +10,7 @@ export default class Comment extends React.Component {
     render() {
         return <li className="list-group-item">
             {this.props.author}:{this.props.children}
-            <span className="pull-right">{this.props.date}</span>
+            <span className="pull-right">{moment(this.props.date).fromNow()}</span>
             <span className="pull-right">
                 <span onClick={this.remove.bind(this)} className="glyphicon glyphicon-remove"></span>
             </span>
